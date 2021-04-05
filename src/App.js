965b3b37454b10cb65189faa6ed23da78d70c0e1
Alfred Meng <alfredfmeng@gmail.com>
2021-04-05 16:20:49 -0500

    initialized firebase project in app and began working on SignIn/SignOut components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
+import "./App.css";
+
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBon4Yj5A2ixasH_7hRbiyqG4uFTbFGQj0",
+  authDomain: "chatapp-e11ed.firebaseapp.com",
+  projectId: "chatapp-e11ed",
+  storageBucket: "chatapp-e11ed.appspot.com",
+  messagingSenderId: "806935058819",
+  appId: "1:806935058819:web:ed683aff7d8b7219b3beb6",
+  measurementId: "G-TL59J25W23",
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>⚛️🔥💬</h1>
+        <SignOut />
       </header>
+
+      <section>{user ? <ChatRoom /> : <SignIn />}</section>
     </div>
   );
 }
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  };
+
+  return <button onClick={signInWithGoogle}>Sign in with Google</button>;
+}
+
+function SignOut() {
+  return (
+    auth.currentUser && <button onClick={() => auth.signOut()}>Sign Out</button>
+  );
+}
+function ChatRoom() {}
+function ChatMessage() {}
 
 export default App;
