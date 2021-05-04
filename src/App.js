01aaba845cdd653366fac711cb125ecd103b4130
Alfred Meng <alfredfmeng@gmail.com>
2021-05-04 15:01:37 -0500

    implemented ability to read chat messages

--- src/App.js
+++ src/App.js
@@ -53,7 +53,25 @@ function SignOut() {
 function ChatRoom() {
   const messagesRef = firestore.collection("messages");
   const query = messagesRef.orderBy("createdAt").limitToLast(25);
+  const [messages] = useCollectionData(query, { idField: "id" });
+  return (
+    <main>
+      {messages &&
+        messages.map((msg) => {
+          <ChatMessage key={msg.id} message={msg} />;
+        })}
+    </main>
+  );
 }
-function ChatMessage() {}
+function ChatMessage(props) {
+  const { text, uid, photoURL } = props.message;
+  const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
+  return (
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL} />
+      <p>{text}</p>
+    </div>
+  );
+}
 
 export default App;
